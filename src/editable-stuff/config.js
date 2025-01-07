e276aaf4b898c18da7006aa9ba9a3328a58cc86d
hyuzukirmizi <hyuzukirmizi@umass.edu>
2025-01-07 10:31:49 -0500

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -85,27 +85,17 @@ const leadership = {
 
 // SKILLS SECTION
 const skills = {
-  show: false,
+  show: true,
   heading: "Skills",
   hardSkills: [
     { name: "Python", value: 100 },
     { name: "Java", value: 100 },
-    { name: "Data Structures", value: 95 },
+    { name: "Data Structures", value: 100 },
     { name: "C", value: 90 },
     { name: "Object Oriented Programming", value: 100 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
-  ],
-  softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
+    { name: "ServiceNow", value: 90 },
+    { name: "KACE", value: 80 },
+    { name: "Microsoft Office", value: 100 },
   ],
 };
 
